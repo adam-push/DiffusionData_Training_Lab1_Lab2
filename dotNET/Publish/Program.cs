f84834652281e1b28429ff86230af10734ac76a5
Daryl Dudey <122302586+ddudey-ddata@users.noreply.github.com>
2023-01-16 12:47:47 +0000

    Initial .NET PubSub

---
+++ dotNET/Publish/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using PushTechnology.ClientInterface.Client.Factories;
+using PushTechnology.ClientInterface.Client.Features;
+using PushTechnology.ClientInterface.Client.Topics;
+
+namespace PubSub
+{
+    class Program
+    {
+        static string Host = "localhost";
+        static int Port = 8080;
+        static string Principal = "admin";
+        static string Credential = "password";
+
+        static async Task Main(string[] args)
+        {
+            // Add a topic and set its value.
+            var session = Diffusion.Sessions
+                .Principal(Principal)
+                .Password(Credential)
+                .Open("ws://"+Host+":"+Port.ToString());
+
+            await session.TopicControl.AddTopicAsync( "my/first/topic", TopicType.STRING);
+
+            int i = 0;
+            while (true)
+            {
+                var stringDataType = Diffusion.DataTypes.String;
+                var msg = "Hello World - " + i.ToString();
+                Console.WriteLine(msg);
+                var result = session.TopicUpdate.SetAsync(
+                    "my/first/topic",
+                    msg);
+                i++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ dotNET/Subscribe/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using PushTechnology.ClientInterface.Client.Callbacks;
+using PushTechnology.ClientInterface.Client.Factories;
+using PushTechnology.ClientInterface.Client.Features;
+using PushTechnology.ClientInterface.Client.Features.Topics;
+using PushTechnology.ClientInterface.Client.Topics.Details;
+using PushTechnology.ClientInterface.Data.JSON;
+
+namespace Subscribe
+{
+    class Program
+    {
+        static string Host = "localhost";
+        static int Port = 8080;
+        static string Principal = "admin";
+        static string Credential = "password";
+
+        static async Task Main(string[] args)
+        {
+            //Subscribe to topics.
+            var session = Diffusion.Sessions
+                .Principal(Principal)
+                .Password(Credential)
+                .Open("ws://" + Host + ":" + Port.ToString());
+
+            session.Topics.AddStream("my/first/topic", new ExampleValueStream());
+            session.Topics.SubscribeAsync("my/first/topic").Wait();
+            while (true)
+            {
+                Thread.Sleep(100);
+            }
+        }
+    }
+
+    class ExampleValueStream : IValueStream<string>
+    {
+        public void OnValue(string topicPath, ITopicSpecification specification,
+            string oldValue, string newValue)
+        {
+            Console.WriteLine(newValue);
+        }
+
+        public void OnError(ErrorReason errorReason)
+        {
+            if (!ErrorReason.SESSION_CLOSED.Equals(errorReason))
+            {
+                Console.WriteLine($"OnError {errorReason}");
+            }
+        }
+
+        public void OnSubscription(string topicPath, ITopicSpecification specification)
+        {
+            Console.WriteLine($"Subscribed to: {topicPath}");
+        }
+
+        public void OnUnsubscription(string topicPath, ITopicSpecification specification, TopicUnsubscribeReason reason)
+        {
+            Console.WriteLine($"Unsubscribed from: {topicPath}");
+        }
+
+        public void OnValue(string topicPath, ITopicSpecification specification, IJSON oldValue, IJSON newValue)
+        {
+            Console.WriteLine($"{topicPath}: {newValue.ToJSONString()}");
+        }
+
+        public void OnClose()
+        {
+            // Not used
+        }
+    }
+}
+
